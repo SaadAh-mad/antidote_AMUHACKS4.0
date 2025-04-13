39cfe79fffdd391aac322aecfe950e57da460f84
fatimaarif19 <fatima.arif13071989@gmail.com>
2025-04-13 15:04:43 +0530

    modified landing

--- kaamvala/app/page.tsx
+++ kaamvala/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Nav from "@/components/navbar";
+import LandingPage from "@/components/landing";
 
 export default function App() {
   return (
     <div>
       <Nav/>
+      <LandingPage/>
     </div>
   );
 }
